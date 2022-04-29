af9f8040f4bc4a72f167d4542dec17a03e32d5dd
jsjune <avraskio@gmail.com>
2022-04-30 02:42:34 +0900

    test

--- src/main/java/com/project/sangil_be/initDb.java
+++ src/main/java/com/project/sangil_be/initDb.java
@@ -7,7 +7,6 @@ import com.project.sangil_be.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-//
 
 @Component
 @RequiredArgsConstructor
