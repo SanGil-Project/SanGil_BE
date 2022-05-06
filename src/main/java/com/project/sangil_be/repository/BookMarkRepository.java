4ea1ed570110c1bf7fc30942da2fc3dc0a5aed59
uihyeon1229 <101084941+uihyeon1229@users.noreply.github.com>
2022-05-07 03:16:19 +0900

    메인페이지 Plan parties mountains 추가

--- src/main/java/com/project/sangil_be/repository/BookMarkRepository.java
+++ src/main/java/com/project/sangil_be/repository/BookMarkRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface BookMarkRepository extends JpaRepository <BookMark, Long> {
 
     BookMark findByMountain100IdAndUserId(Long mountainId, Long userId);
+
+    boolean existsByMountain100IdAndUserId(Long mountain100Id, Long userId);
 }
